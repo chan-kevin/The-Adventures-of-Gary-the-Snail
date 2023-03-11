f55ef7734e90c39153bb3993b47a4467e10b433e
Kevin Chan <chankevin13@gmail.com>
2023-03-11 01:37:18 -0500

    moving

--- src/scripts/gary.js
+++ src/scripts/gary.js
@@ -6,9 +6,14 @@ export default class Gary {
         this.width = width;
         this.height = height;
         this.x = 10;
-        this.image_width = 170;
+        this.image_width = 80;
         this.image_height = 115;
-        this.image = document.getElementById("gary")
+        // this.image_width = 00;
+        // this.image_height = 200;
+        this.gameFrame = 0;
+        this.staggerFrames = 100;
+        this.image_num = 0;
+        this.image = [document.getElementById("gary"), document.getElementById("gary2"), document.getElementById("gary3")]
         this.drawIt();
     }
 
@@ -17,8 +22,17 @@ export default class Gary {
         new Frame(this.ctx, this.width, this.height);
         // this.ctx.fillStyle = "red"; 
         // this.ctx.fillRect(this.x,450,200,100); 
-        this.ctx.drawImage(this.image, this.image_width, this.image_height, 350, 400, this.x, 500, 80, 100);
-        this.x+=5; 
-        window.requestAnimationFrame(this.drawIt.bind(this));
+        // this.ctx.drawImage(this.image, this.image_width, this.image_height, 350, 400, this.x, 500, 80, 100);
+        this.ctx.drawImage(this.image[this.image_num], this.image_width, this.image_height, 380, 430, this.x, 500, 80, 100);
+        // if (this.frame_x < 3) this.frame_x ++;
+        // else this.frame_x = 1;
+        // if (this.gameFrame % this.staggerFrames == 0){
+        setTimeout(() => {
+            if (this.image_num < 2) this.image_num ++;
+            else this.image_num = 0; 
+            this.x+=5;
+            window.requestAnimationFrame(this.drawIt.bind(this));}, "90"
+        )
+        // }
     } 
 }
