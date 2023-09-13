fc4cf7c835425f870cc64a7916043d670e2db831
Kevin Chan <chankevin13@gmail.com>
2023-09-13 12:30:45 -0400

    retry delpoy

--- src/scripts/gameover.js
+++ src/scripts/gameover.js
@@ -13,10 +13,12 @@ export default class Gameover {
             this.game.gary.zap.pause();
             this.overzap.style.display = "block";
             this.gamebackground.style.display = "none";
+            
             if (this.game.gary.hit) {
                 this.game.gary.zap.loop = true;
                 this.game.gary.zap.play();
             }
+
             this.game.swap.removeSwap();
             this.game.gary.removeSongToggle();
             document.body.addEventListener("keydown", startGame);
